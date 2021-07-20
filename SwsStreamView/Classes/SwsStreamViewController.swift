e5090d35b7f1a358eeaf9dde91a2dadb44800dba
Inpyo Hong <y2kpaulh@epiens.com>
2021-07-20 12:17:45 +0900

    -

--- SwsStreamView/Classes/SwsStreamViewController.swift
+++ SwsStreamView/Classes/SwsStreamViewController.swift
@@ -7,29 +7,3 @@ class SwsStreamViewController: UIViewController {
         
     }
 }
-
-internal class HiddenViewController: UIViewController {
-
-    internal override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-    
-    internal func testFunc() {
-        
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}

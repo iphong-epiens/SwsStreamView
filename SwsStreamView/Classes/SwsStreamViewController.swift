cef4c046588995615d0cfd08d5d903827a5a1d0e
Inpyo Hong <y2kpaulh@epiens.com>
2021-07-20 12:12:49 +0900

    hidden vc 삭제

--- SwsStreamView/Classes/SwsStreamViewController.swift
+++ SwsStreamView/Classes/SwsStreamViewController.swift
@@ -7,3 +7,31 @@ class SwsStreamViewController: UIViewController {
         
     }
 }
+
+internal class HiddenViewController: UIViewController {
+
+    internal override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.ba
+        // Do any additional setup after loading the view.
+    }
+    
+    internal func testFunc() {
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

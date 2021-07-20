7332454bc85be1f3b57283b5d91aba819261170a
Inpyo Hong <y2kpaulh@epiens.com>
2021-07-20 12:08:22 +0900

    internal class 호출 루틴 삭제

--- SwsStreamView/Classes/SwsStreamViewController.swift
+++ SwsStreamView/Classes/SwsStreamViewController.swift
@@ -4,7 +4,6 @@ import Alamofire
 
 class SwsStreamViewController: UIViewController {
     override func viewDidLoad() {
-        let vc = HiddenViewController()
-        self.present(vc, animated: true, completion: nil)
+        
     }
 }
